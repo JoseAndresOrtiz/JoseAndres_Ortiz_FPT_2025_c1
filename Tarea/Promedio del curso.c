a2eb42ff2a4b0f8351726b393c4503d5ed0f356e
JoseAndresOrtiz <20250145@itla.edu.do>
2025-02-06 21:25:47 -0400

    Add files via upload

---
+++ Tarea/Promedio del curso.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(void)
+{
+float PRO;
+ printf("Ingrese el promedio del curso:");
+scanf("%f",&PRO);
+if (PRO >= 6.0)
+    printf("\nAprobado");
+else
+    printf("\nReprobado");
+}
